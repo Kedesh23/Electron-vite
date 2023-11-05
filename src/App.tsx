f91f5919b8f7ff60b6c93dd9408af53377574db8
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-11-05 14:31:30 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Hello Sevan, this my first window with Electron</h1>
+      <h1 className='font-bold text-5xl'>Hello Sevan, this my first window with Electron</h1>
     </>
   )
 }
